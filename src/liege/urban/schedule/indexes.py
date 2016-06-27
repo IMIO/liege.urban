9151f8e0ec96d7c456c8e87b76cb85b15b44b256
Simon Delcourt <delcourt.simon@gmail.com>
2016-06-27 19:58:38 +0000

    index shore on tasks

--- src/liege/urban/schedule/indexes.py
+++ src/liege/urban/schedule/indexes.py
@@ -6,7 +6,7 @@ from plone.indexer import indexer
 
 
 @indexer(IAutomatedTask)
-def licence_reference_index(task):
+def task_reference_index(task):
     """
     Index licence reference on their tasks to be able
     to query on it.
@@ -14,3 +14,14 @@ def licence_reference_index(task):
     licence = task.get_container()
     reference = licence.getReference()
     return reference
+
+
+@indexer(IAutomatedTask)
+def task_shore_index(task):
+    """
+    Index licence shore on their tasks to be able
+    to query on it.
+    """
+    licence = task.get_container()
+    shore = licence.getShore()
+    return shore
