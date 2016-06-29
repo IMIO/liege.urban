afad553505a11eb725ee2c0098ff653b66713c9f
Simon Delcourt <delcourt.simon@gmail.com>
2016-06-29 20:35:39 +0000

    add composition field

--- src/liege/urban/content/buildlicence.py
+++ src/liege/urban/content/buildlicence.py
@@ -1,22 +1,73 @@
 # -*- coding: utf-8 -*-
 
 from Products.Archetypes.atapi import Schema
+from Products.Archetypes.atapi import StringField
+from Products.Archetypes.public import DisplayList
+
+from Products.MasterSelectWidget.MasterSelectWidget import MasterSelectWidget
 
 from Products.urban.BuildLicence import BuildLicence
 
 
+slave_fields_composition = (
+    {
+        'name': 'missingParts',
+        'action': 'vocabulary',
+        'vocab_method': 'getCompositionMissingParts',
+        'control_param': 'composition',
+    },
+)
+
+
 def update_item_schema(baseSchema):
 
-    specificSchema = Schema((),)
+    specificSchema = Schema((
+        StringField(
+            name='composition',
+            widget=MasterSelectWidget(
+                slave_fields=slave_fields_composition,
+                label='Composition',
+                label_msgid='urban_label_composition',
+                i18n_domain='urban',
+            ),
+            schemata='urban_description',
+            vocabulary='listComposition',
+        ),
+    ),
+    )
     BuildLicenceSchema = baseSchema + specificSchema.copy()
 
+    BuildLicenceSchema['missingParts'].widget.format = None
     # stats INS no longer mandatory
     BuildLicenceSchema['usage'].required = False
     BuildLicenceSchema['roadTechnicalAdvice'].widget.label_msgid = 'urban_label_roadDescription'
+    BuildLicenceSchema.moveField('composition', after='workType')
 
     return BuildLicenceSchema
 
 
+def getCompositionMissingParts(self, composition):
+    """
+    """
+    urban_voc = self.schema['missingParts'].vocabulary
+    all_terms = urban_voc.getAllVocTerms(self)
+
+    display_values = [(term.Title().decode('utf-8'), id) for id, term in all_terms.iteritems() if str(composition) in term.getExtraValue()]
+
+    return DisplayList(display_values)
+
+
+def listComposition(self):
+    vocab = (
+        ('285', 'Art. 285 - complet avec architecte'),
+        ('288', 'Art. 288 - simplifié avec architecte'),
+        ('291', 'Art. 291 - simplifié sans architecte'),
+    )
+    return DisplayList(vocab)
+
+
+BuildLicence.getCompositionMissingParts = getCompositionMissingParts
+BuildLicence.listComposition = listComposition
 BuildLicence.schema = update_item_schema(BuildLicence.schema)
 
 
