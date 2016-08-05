7782ad494cb793254cdf0b3fc51f64b0ae000480
Simon Delcourt <delcourt.simon@gmail.com>
2016-08-05 09:46:39 +0000

    add portal type event FD opinion to add custom workflow on FD opinion request

--- src/liege/urban/interfaces.py
+++ src/liege/urban/interfaces.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from zope.interface import Interface
+
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ILiegeUrbanLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IFDOpinionEvent(Interface):
+    """IFDOpinion type marker interface"""
