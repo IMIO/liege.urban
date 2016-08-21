b5e2f8e5cdb1ba4a8a5a4c70feec260ff297247a
Simon Delcourt <delcourt.simon@gmail.com>
2016-08-21 19:56:02 +0000

    revert services init

--- src/liege/urban/services/__init__.py
+++ src/liege/urban/services/__init__.py
@@ -3,8 +3,8 @@
 from Products.urban.config import ExternalConfig
 from liege.urban.services.address import LiegeAddressService
 
-#try:
-#    config = ExternalConfig('services')
-#except:
-config = {}
+try:
+    config = ExternalConfig('services')
+except:
+    config = {}
 liege_address = LiegeAddressService(**(config and config.liege_address))
