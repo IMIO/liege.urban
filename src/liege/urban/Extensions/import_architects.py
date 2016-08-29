d6c5ff32e9ec68795cb214a5262e1510e4744832
Simon Delcourt <delcourt.simon@gmail.com>
2016-08-29 14:04:26 +0000

    extract street number

--- src/liege/urban/Extensions/import_architects.py
+++ src/liege/urban/Extensions/import_architects.py
@@ -3,6 +3,7 @@
 from plone import api
 
 import csv
+import re
 
 
 def do_import():
@@ -14,5 +15,14 @@ def do_import():
         row.pop('')
         for k, v in row.iteritems():
             row[k] = v.decode('iso-8859-1')
+
+        number_match = re.search('(\D*)(\d.*)', row['street'])
+        if number_match:
+            row['street'] = number_match.groups()[0]
+            old_number = row['number']
+            if old_number:
+                row['number'] = u'{} {}'.format(old_number, number_match.groups()[1])
+            else:
+                row['number'] = number_match.groups()[1]
         archi_id = site.plone_utils.normalizeString(u'{}-{}'.format(row['name1'], row['name2']))
         archi_folder.invokeFactory('Architect', id=site.generateUniqueId(archi_id), **row)
