7b67992ff33358e57b0b9e2828395b87ed670295
Franck Ngaha <franck.o.ngaha@gmail.com>
2016-10-06 11:52:35 +0000

    more view method

--- src/liege/urban/documentgenerator/helper_view.py
+++ src/liege/urban/documentgenerator/helper_view.py
@@ -6,6 +6,8 @@ from liege.urban.interfaces import IShore
 
 from zope.component import queryAdapter
 
+from plone import api
+
 
 class LiegeLicenceProxyObject(ATDisplayProxyObject):
     """
@@ -26,3 +28,45 @@ class LiegeLicenceProxyObject(ATDisplayProxyObject):
         licence = self.context
         to_shore = queryAdapter(licence, IShore)
         return to_shore.display()
+
+    def get_work_location_signaletic(self, separator=', '):
+        """
+        Adresse(s) des travaux (workLocations)
+        """
+        licence = self.context
+        workLocations = licence.getWorkLocations()
+        workLocation_signaletic = self._get_work_location_signaletic(workLocations[0])
+        for workLocation in workLocations[1:]:
+            workLocation_signaletic += separator + self.get_work_location_signaletic(workLocation)
+        return workLocation_signaletic
+
+    def _get_work_location_signaletic(self, workLocation):
+        catalog = api.portal.get_tool("uid_catalog")
+        street = catalog(UID=workLocation['street'])[0].getObject()
+        return street.getStreetName() + ' ' +  workLocation['number']
+
+    def get_work_location(self, index):
+        """
+        return a dictionary containing all work locations informations
+        """
+        work_location = {
+                'bestAddressKey': '',
+                'streetCode': '',
+                'streetName': '',
+                'startDate': '',
+                'endDate': '',
+                'regionalRoad': '',
+                'number': '',
+        }
+        licence = self.context
+        workLocation = licence.getWorkLocations()[index]
+        catalog = api.portal.get_tool("uid_catalog")
+        street = catalog(UID=workLocation['street'])[0].getObject()
+        work_location['bestAddressKey'] = street.getBestAddressKey()
+        work_location['streetCode'] = street.getStreetCode()
+        work_location['streetName'] = street.getStreetName()
+        work_location['startDate'] = street.getStartDate()
+        work_location['endDate'] = street.getEndDate()
+        work_location['regionalRoad'] = street.getRegionalRoad()
+        work_location['number'] =  workLocation['number']
+        return work_location
