b19d3644c33531bcd4ec42ea71c110e6db92f18a
Simon Delcourt <delcourt.simon@gmail.com>
2016-10-10 11:36:02 +0000

    set permissions fields for road grous and external readers groups

--- src/liege/urban/licence_fields_permissions.py
+++ src/liege/urban/licence_fields_permissions.py
@@ -8,8 +8,24 @@ def init_field_permissions():
     """
     schemas = [BuildLicence_schema]
 
+    permissions_mapping = {
+        'urban_description': ('liege.urban: External Reader', ''),
+        'urban_advices': ('liege.urban: Road Reader', ''),
+        'urban_inquiry': ('', ''),
+        'urban_analysis': ('', ''),
+        'urban_location': ('liege.urban: Road Reader', 'liege.urban: Road Editor'),
+        'urban_road': ('liege.urban: Road Reader', 'liege.urban: Road Editor'),
+        'urban_habitation':  ('', ''),
+        'urban_peb':  ('', ''),
+    }
+
+    exceptions = ['portal_type']
+
     for schema in schemas:
         for field in schema.fields():
-            exceptions = ['portal_type']
-            if field.schemata != 'urban_description' and field.getName() not in exceptions:
-                field.read_permission = 'liege.urban: Internal Reader'
+            if field.getName() not in exceptions:
+                read_permission, write_permission = permissions_mapping.get(field.schemata, ('', ''))
+                if read_permission:
+                    field.read_permission = read_permission
+                if write_permission:
+                    field.write_permission = write_permission
