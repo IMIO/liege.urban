01030782aaad4822b0db814879eb63caecc28462
Franck Ngaha <franck.o.ngaha@gmail.com>
2016-10-27 08:46:09 +0000

    refactor proxy

--- src/liege/urban/documentgenerator/helper_view.py
+++ src/liege/urban/documentgenerator/helper_view.py
@@ -14,6 +14,11 @@ class LiegeLicenceProxyObject(ATDisplayProxyObject):
     Archetypes implementation of DisplayProxyObject.
     """
 
+    def getShore(self):
+        licence = self.context
+        to_shore = queryAdapter(licence, IShore)
+        return to_shore.display()
+
     @property
     def reference(self):
         """
@@ -23,50 +28,3 @@ class LiegeLicenceProxyObject(ATDisplayProxyObject):
         to_shore = queryAdapter(licence, IShore)
         ref = '{} {}'.format(licence.reference, to_shore.display())
         return ref
-
-    def getShore(self):
-        licence = self.context
-        to_shore = queryAdapter(licence, IShore)
-        return to_shore.display()
-
-    def get_work_location_signaletic(self, separator=', '):
-        """
-        Adresse(s) des travaux (workLocations)
-        """
-        licence = self.context
-        workLocations = licence.getWorkLocations()
-        workLocation_signaletic = self._get_work_location_signaletic(workLocations[0])
-        for workLocation in workLocations[1:]:
-            workLocation_signaletic += separator + self.get_work_location_signaletic(workLocation)
-        return workLocation_signaletic
-
-    def _get_work_location_signaletic(self, workLocation):
-        catalog = api.portal.get_tool("uid_catalog")
-        street = catalog(UID=workLocation['street'])[0].getObject()
-        return street.getStreetName() + ' ' +  workLocation['number']
-
-    def get_work_location(self, index):
-        """
-        return a dictionary containing all work locations informations
-        """
-        work_location = {
-                'bestAddressKey': '',
-                'streetCode': '',
-                'streetName': '',
-                'startDate': '',
-                'endDate': '',
-                'regionalRoad': '',
-                'number': '',
-        }
-        licence = self.context
-        workLocation = licence.getWorkLocations()[index]
-        catalog = api.portal.get_tool("uid_catalog")
-        street = catalog(UID=workLocation['street'])[0].getObject()
-        work_location['bestAddressKey'] = street.getBestAddressKey()
-        work_location['streetCode'] = street.getStreetCode()
-        work_location['streetName'] = street.getStreetName()
-        work_location['startDate'] = street.getStartDate()
-        work_location['endDate'] = street.getEndDate()
-        work_location['regionalRoad'] = street.getRegionalRoad()
-        work_location['number'] =  workLocation['number']
-        return work_location
