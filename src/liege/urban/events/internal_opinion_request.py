164479bfb335a4500dc9681cc932ff3d563d61a2
Simon Delcourt <delcourt.simon@gmail.com>
2016-11-29 14:13:26 +0000

    set internal opinion request date automatically from workflow actions

---
+++ src/liege/urban/events/internal_opinion_request.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def set_dates(urban_event, event):
+    """
+    """
+    ask_date = None
+    opinion_date = None
+
+    for wf_action in urban_event.workflow_history['opinion_request_workflow']:
+        if wf_action['action'] == 'ask_opinion':
+            ask_date = wf_action['time']
+        elif wf_action['action'] == 'send_opinion':
+            opinion_date = wf_action['time']
+
+    urban_event.setEventDate(ask_date)
+    urban_event.setReceiptDate(opinion_date)
