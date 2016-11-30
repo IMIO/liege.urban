bad6f3460fad486c93ba88811f8cca1a90cf8bfe
Simon Delcourt <delcourt.simon@gmail.com>
2016-11-30 09:10:49 +0000

    complete eventype mapper for opinion request creation

--- src/liege/urban/setuphandlers.py
+++ src/liege/urban/setuphandlers.py
@@ -32,6 +32,7 @@ def setEventTypeMapping(context):
     portal_urban = api.portal.get_tool('portal_urban')
     portal_urban.eventtype_portaltype_mapping['Products.urban.interfaces.IWalloonRegionOpinionRequestEvent'] = 'UrbanEventFDOpinion'
     portal_urban.eventtype_portaltype_mapping['Products.urban.interfaces.IAcknowledgmentEvent'] = 'UrbanEventAcknowledgment'
+    portal_urban.eventtype_portaltype_mapping['liege.urban.interfaces.IInternalOpinionRequestEvent'] = 'UrbanEventOpinionRequest'
 
 
 def addLiegeGroups(context):
