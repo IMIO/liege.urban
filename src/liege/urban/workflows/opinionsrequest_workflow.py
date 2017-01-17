ee644a2fca5e2d7e946701da4566b0ba7d3f9a2c
Simon Delcourt <delcourt.simon@gmail.com>
2017-01-17 09:16:47 +0000

    fix opinion request role for opinion maker

--- src/liege/urban/workflows/opinionsrequest_workflow.py
+++ src/liege/urban/workflows/opinionsrequest_workflow.py
@@ -11,7 +11,7 @@ class StateRolesMapping(LocalRoleAdapter):
     """
     """
 
-    def get_opinion_editor(self):
+    def get_opinion_group(self, groupe_type='editors'):
         opinion_request = self.context
 
         portal_urban = api.portal.get_tool('portal_urban')
@@ -20,14 +20,22 @@ class StateRolesMapping(LocalRoleAdapter):
         task = None
         for task_config in schedule_config.get_all_task_configs():
             for obj in opinion_request.objectValues():
-                if IAutomatedTask.providedBy(obj) and obj.task_config_UID == task_config.UID():
-                    task = obj
-                    break
+                is_task = IAutomatedTask.providedBy(obj)
+                if is_task and obj.task_config_UID == task_config.UID():
+                    if obj.assigned_group.endswith(groupe_type):
+                        task = obj
+                        break
 
         if task:
             return (task.assigned_group,)
 
         return ('technical_editors',)
+
+    def get_opinion_editor(self):
+        return self.get_opinion_group('editors')
+
+    def get_opinion_validator(self):
+        return self.get_opinion_group('validators')
 
     def get_opinion_editor_role(self):
         groups = self.get_opinion_editor()
@@ -36,7 +44,7 @@ class StateRolesMapping(LocalRoleAdapter):
         return ('Editor',)
 
     def get_technical_roles(self):
-        if  'technical_editors' in self.get_opinion_editor():
+        if 'technical_editors' in self.get_opinion_editor():
             return ('Contributor',)
         return ('Reader',)
 
@@ -54,6 +62,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': (get_technical_roles,),
             'technical_validators': ('Reader',),
             get_opinion_editor: (get_opinion_editor_role,),
+            get_opinion_validator: (get_opinion_editor_role,),
         },
 
         'opinion_validation': {
@@ -61,7 +70,8 @@ class StateRolesMapping(LocalRoleAdapter):
             'administrative_validators': ('Reader',),
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
-            get_opinion_editor: ('Editor',),
+            get_opinion_editor: ('Reader',),
+            get_opinion_validator: ('Contributor',),
         },
 
         'opinion_given': {
@@ -70,6 +80,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
             get_opinion_editor: ('Reader',),
+            get_opinion_validator: ('Reader',),
         },
 
     }
