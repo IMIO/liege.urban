ed9b8c1d3cabfaabe5e144f250f4c4e57215e30e
Simon Delcourt <delcourt.simon@gmail.com>
2017-01-26 08:54:46 +0000

    typo

--- src/liege/urban/workflows/buildlicence_workflow.py
+++ src/liege/urban/workflows/buildlicence_workflow.py
@@ -46,7 +46,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'administrative_validators': ('Contributor',),
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'validating_address': {
@@ -55,7 +55,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
             'survey_editors': ('Reader', 'AddressEditor'),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'waiting_address': {
@@ -64,7 +64,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
             'survey_editors': ('Reader', 'AddressEditor'),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'procedure_choice': {
@@ -73,7 +73,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Editor',),
             'technical_validators': ('Contributor',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'checking_completion': {
@@ -82,7 +82,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Editor',),
             'technical_validators': ('Contributor',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'incomplete': {
@@ -91,7 +91,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'complete': {
@@ -100,7 +100,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Editor',),
             'technical_validators': ('Contributor',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'procedure_choosen': {
@@ -109,7 +109,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Reader',),
             'technical_validators': ('Contributor',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'procedure_validated': {
@@ -120,7 +120,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'Voirie_editors': ('RoadEditor',),
             'Voirie_validators': ('RoadEditor',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'report_written': {
@@ -131,7 +131,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'Voirie_editors': ('RoadEditor',),
             'Voirie_validators': ('RoadEditor',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'FD_opinion': {
@@ -142,7 +142,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'Voirie_editors': ('RoadEditor',),
             'Voirie_validators': ('RoadEditor',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'decision_in_progress': {
@@ -153,7 +153,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'Voirie_editors': ('RoadReader',),
             'Voirie_validators': ('RoadReader',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'authorized': {
@@ -164,7 +164,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'Voirie_editors': ('RoadReader',),
             'Voirie_validators': ('RoadReader',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'refused': {
@@ -175,7 +175,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'Voirie_editors': ('RoadReader',),
             'Voirie_validators': ('RoadReader',),
             get_opinion_editors: ('ExternalReader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
     }
--- src/liege/urban/workflows/preliminarynotice_workflow.py
+++ src/liege/urban/workflows/preliminarynotice_workflow.py
@@ -12,7 +12,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'administrative_validators': ('Contributor', 'AddressEditor'),
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'analysis': {
@@ -20,7 +20,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'administrative_validators': ('Reader',),
             'technical_editors': ('Editor',),
             'technical_validators': ('Contributor',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'analysis_validation': {
@@ -28,7 +28,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'administrative_validators': ('Reader',),
             'technical_editors': ('Reader',),
             'technical_validators': ('Contributor',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'college': {
@@ -36,7 +36,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'administrative_validators': ('Contributor',),
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'favorable': {
@@ -44,7 +44,7 @@ class StateRolesMapping(LocalRoleAdapter):
             'administrative_validators': ('Reader',),
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
 
         'defavorable': {
@@ -52,6 +52,6 @@ class StateRolesMapping(LocalRoleAdapter):
             'administrative_validators': ('Reader',),
             'technical_editors': ('Reader',),
             'technical_validators': ('Reader',),
-            'urban_reader': ('Reader',),
+            'urban_readers': ('Reader',),
         },
     }
