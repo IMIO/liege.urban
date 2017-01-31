b3ac05463f5f52a78c717329eee6c4c25c0f8b57
Simon Delcourt <delcourt.simon@gmail.com>
2017-01-31 13:31:06 +0000

    add default workflow local roles for urban event

---
+++ src/liege/urban/workflows/urbanevent_workflow.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from liege.urban.workflows.adapter import LocalRoleAdapter
+
+
+class StateRolesMapping(LocalRoleAdapter):
+    """
+    """
+
+    mapping = {
+        'in_progress': {
+            'administrative_editors': ('Editor',),
+            'administrative_validators': ('Editor', 'Contributor'),
+            'technical_editors': ('Editor',),
+            'technical_validators': ('Editor', 'Contributor'),
+        },
+
+        'closed': {
+            'administrative_editors': ('Reader',),
+            'administrative_validators': ('Reader',),
+            'technical_editors': ('Reader',),
+            'technical_validators': ('Reader',),
+        },
+    }
