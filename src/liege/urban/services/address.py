1ebe7845f4403d5a3875638907e03e8a437fba4e
Simon Delcourt <delcourt.simon@gmail.com>
2017-04-06 11:30:29 +0000

    remove useless method

--- src/liege/urban/services/address.py
+++ src/liege/urban/services/address.py
@@ -24,7 +24,6 @@ class LiegeAddressService(SQLService):
                 'ptadresses_vdl',
                 column_names=[
                     'gid',
-                    'g_legnum',
                     'secteururb',
                     'num_cad_a_',
                     'coderue',
@@ -69,20 +68,6 @@ class LiegeAddressSession(SQLSession):
         query = self._base_query_address()
 
         query = query.filter(table.gid == gid)
-        try:
-            address = query.distinct().one()
-            return address
-        except:
-            return
-
-    def query_address_by_legnum(self, legnum):
-        """
-        Return the unique 'gid' address point .
-        """
-        table = self.tables.ptadresses_vdl
-        query = self._base_query_address()
-
-        query = query.filter(table.g_legnum == legnum)
         try:
             address = query.distinct().one()
             return address
