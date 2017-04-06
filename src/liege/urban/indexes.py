1705678d52eeec3a84400bced601d0b5e2bad305
Simon Delcourt <delcourt.simon@gmail.com>
2017-04-06 21:14:40 +0000

    custom decision date index for liege buildlicence and 127

--- src/liege/urban/indexes.py
+++ src/liege/urban/indexes.py
@@ -4,7 +4,10 @@ from liege.urban.interfaces import IShore
 
 from plone.indexer import indexer
 
+from Products.urban.interfaces import IArticle127
+from Products.urban.interfaces import IBuildLicence
 from Products.urban.interfaces import IGenericLicence
+from Products.urban.interfaces import ISimpleCollegeEvent
 
 from zope.component import queryAdapter
 
@@ -17,3 +20,17 @@ def genericlicence_shore_index(licence):
     adapter = queryAdapter(licence, IShore)
     shore = adapter.get_shore()
     return shore
+
+
+@indexer(IArticle127)
+def article127_decisiondate(licence):
+    decision_event = licence._getLastEvent(ISimpleCollegeEvent, use_catalog=False)
+    if decision_event:
+        return decision_event.getEventDate()
+
+
+@indexer(IBuildLicence)
+def buillicence_decisiondate(licence):
+    decision_event = licence._getLastEvent(ISimpleCollegeEvent, use_catalog=False)
+    if decision_event:
+        return decision_event.getEventDate()
