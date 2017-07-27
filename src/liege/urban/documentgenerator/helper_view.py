d6bc5ec75c8acff08569c5121e42a20c3d215eb8
Franck Ngaha <franck.o.ngaha@gmail.com>
2017-07-27 14:00:33 +0000

    more helper
    Provide a way to check if the licence is authorized

--- src/liege/urban/documentgenerator/helper_view.py
+++ src/liege/urban/documentgenerator/helper_view.py
@@ -2,6 +2,7 @@
 
 from liege.urban.interfaces import IShore
 
+from Products.CMFCore.utils import getToolByName
 from Products.urban.docgen.helper_view import LicenceDisplayProxyObject
 
 from zope.component import queryAdapter
@@ -11,6 +12,9 @@ class LiegeLicenceProxyObject(LicenceDisplayProxyObject):
     """
     Archetypes implementation of DisplayProxyObject.
     """
+
+    def authorized(self):
+        return self.context.workflow_history['buildlicence_workflow'][-1]['review_state'] == 'authorized'
 
     def getShore(self):
         licence = self.context
