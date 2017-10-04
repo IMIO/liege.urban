a3b384e9ed8e046e017f230a65451eb0e5e50dc3
Simon Delcourt <delcourt.simon@gmail.com>
2017-10-04 12:32:03 +0000

    set tabs permissions for integrated licences

--- src/liege/urban/content/codt_integratedlicence.py
+++ src/liege/urban/content/codt_integratedlicence.py
@@ -2,11 +2,24 @@
 
 from Products.urban.CODT_IntegratedLicence import CODT_IntegratedLicence
 
-# buildlicence and article127 schema should have the same changes
+# buildlicence and integratedlicence schema should have the same changes
 from liege.urban.content.buildlicence import update_item_schema
+from liege.urban.licence_fields_permissions import set_field_permissions
+
+permissions_mapping = {
+    'urban_description': ('liege.urban: External Reader', 'liege.urban: Internal Editor'),
+    'urban_location': ('liege.urban: External Reader', 'liege.urban: Internal Editor'),
+    'urban_road': ('liege.urban: Road Reader', 'liege.urban: Road Editor'),
+    'urban_habitation': ('liege.urban: External Reader', 'urban: Add PortionOut'),
+}
 
 
 CODT_IntegratedLicence.schema = update_item_schema(CODT_IntegratedLicence.schema)
+CODT_IntegratedLicence.schema = set_field_permissions(
+    CODT_IntegratedLicence.schema,
+    permissions_mapping,
+    exceptions=['portal_type']
+)
 
 
 # Classes have already been registered, but we register them again here
