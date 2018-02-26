939d149f1935263c7003a8528dd81a0b936e811d
Simon Delcourt <delcourt.simon@gmail.com>
2018-02-26 07:58:38 +0000

    adapt redirects to let environment users go to the front page by default

--- src/liege/urban/browser/redirects.py
+++ src/liege/urban/browser/redirects.py
@@ -26,6 +26,8 @@ class UrbanRootRedirects(object):
             group_ids = [g.id for g in user_groups]
             if 'survey_editors' in group_ids:
                 return 'urban/survey_schedule'
+            if 'urban_readers' in group_ids or 'environment_readers' in group_ids:
+                return 'urban'
             if 'opinions_editors' in group_ids:
                 return 'urban/opinions_schedule'
             return 'urban'
