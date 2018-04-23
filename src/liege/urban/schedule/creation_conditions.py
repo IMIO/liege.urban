6773464b82659e06c0183b23ee1fc277bbdc644b
Simon Delcourt <delcourt.simon@gmail.com>
2018-04-23 14:36:25 +0200

    Add translations for schedule conditions.

--- src/liege/urban/schedule/creation_conditions.py
+++ src/liege/urban/schedule/creation_conditions.py
@@ -12,3 +12,33 @@ class LicenceInValidatingAddressState(CreationCondition):
     def evaluate(self):
         licence = self.task_container
         return api.content.get_state(licence) == 'validating_address'
+
+
+class PreliminaryAdviceCondition(CreationCondition):
+    """
+    """
+    def __init__(self, licence, task_config):
+        super(PreliminaryAdviceCondition, self).__init__(licence, task_config)
+        self.preliminary_advice_event = licence.getLastInternalPreliminaryAdvice()
+
+
+class PreliminaryAdviceEventCreated(PreliminaryAdviceCondition):
+    """
+    Preliminary advice event is created and proposed to technical validation
+    """
+
+    def evaluate(self):
+        if not self.preliminary_advice_event:
+            return False
+        return api.content.get_state(self.preliminary_advice_event) == 'in_progress'
+
+
+class PreliminaryAdviceWritten(PreliminaryAdviceCondition):
+    """
+    Preliminary advice event is created and proposed to technical validation
+    """
+
+    def evaluate(self):
+        if not self.preliminary_advice_event:
+            return False
+        return api.content.get_state(self.preliminary_advice_event) == 'technical_validation'
