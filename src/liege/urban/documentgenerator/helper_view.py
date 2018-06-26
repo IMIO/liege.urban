4e5504aeb78c999785302c80fb8d19e69ce801f7
Simon Delcourt <delcourt.simon@gmail.com>
2018-06-26 09:05:21 +0200

    Add helper method to distinguish between urban and environment folder managers.

--- src/liege/urban/documentgenerator/helper_view.py
+++ src/liege/urban/documentgenerator/helper_view.py
@@ -32,3 +32,27 @@ class LiegeLicenceProxyObject(LicenceDisplayProxyObject):
         to_shore = queryAdapter(licence, IShore)
         ref = '{} {}'.format(licence.reference, to_shore.display())
         return ref
+
+    def get_first_folder_manager(self, group='', grade=''):
+        """
+        """
+        groups_mapping = {
+            'urban': [
+                'administrative_editors',
+                'administrative_validators',
+                'technical_editors',
+                'technical_validators'
+            ],
+            'environment': [
+                'administrative_editors_environment',
+                'administrative_validators_environment',
+                'technical_editors_environment',
+                'technical_validators_environment'
+            ],
+        }
+        for folder_manager in self.getFoldermanagers():
+            if folder_manager.getGrade() == grade:
+               groups = set([g.id for g in api.group.get_groups(username=folder_manager.getPloneUserId())])
+               if groups_mapping.get(group, None) and groups.intersection(groups_mapping.get(group)):
+                   return folder_manager
+
