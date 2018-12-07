b3a260d16822f7e28a16037f4099d8376aa847bc
Simon Delcourt <delcourt.simon@gmail.com>
2018-12-07 09:28:51 +0100

    Add import script for lieg notaries. See URBLIE-311.

---
+++ src/liege/urban/Extensions/import_notaries.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+
+import csv
+
+
+def do_import():
+    site = api.portal.get()
+    notaries_folder = site.urban.notaries
+    fieldnames = [
+        'id',
+        'name1',
+        'name2',
+        'street1',
+        'street2',
+        'number',
+        'zipcode',
+        'city',
+        'phone',
+        'fax',
+        'title',
+        'disable',
+    ]
+
+    title_maping = {
+        'Notaire': 'master',
+        'Société': 'company',
+    }
+
+    reader = csv.DictReader(open('notaires.csv', 'r'), fieldnames, delimiter=';')
+    notaries_args = [row for row in reader if row['name1']][1:]
+    for notary_arg in notaries_args:
+        notary_arg['street'] = '{} {}'.format(notary_arg.pop('street1'), notary_arg.pop('street2'))
+        notary_arg['personTitle'] = title_maping.get(notary_arg.pop('title'), 'notitle')
+        disable = int(notary_arg.pop('disable'))
+        notaries_folder.invokeFactory('Notary', **notary_arg)
+        if disable:
+            notary = getattr(notaries_folder, notary_arg['id'])
+            api.content.transition(notary, 'disable')
