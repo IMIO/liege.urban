d9f4b47c3268d10be8fa6a82a897540095964b52
Simon Delcourt <delcourt.simon@gmail.com>
2019-01-07 16:03:46 +0100

    Add permissions mapping for survey.

--- src/liege/urban/workflows/roaddecree_workflow.py
+++ src/liege/urban/workflows/roaddecree_workflow.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from liege.urban.workflows.licences_workflow import DefaultStateRolesMapping
+from liege.urban.workflows.licences_workflow import DefaultStateRolesMapping as LiegeBase
 
 
-class StateRolesMapping(DefaultStateRolesMapping):
+class StateRolesMapping(LiegeBase):
     """
     State role mapping adapter for RoadDecree content type
     """
 
     mapping = {
+
+        'validating_address': {
+            LiegeBase.get_opinion_editors: ('ExternalReader',),
+            'survey_editors': ('Reader', 'AddressEditor'),
+            'urban_readers': ('Reader',),
+        },
+
+        'waiting_address': {
+            LiegeBase.get_opinion_editors: ('ExternalReader',),
+            'survey_editors': ('Reader', 'AddressEditor'),
+            'urban_readers': ('Reader',),
+        },
+
         'technical_analysis_post_investigation': {
             'fittingout_technicians': ('Editor', ),
             'fittingout_technicians_validators': ('Contributor', )
