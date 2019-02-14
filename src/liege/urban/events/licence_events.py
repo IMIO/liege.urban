733c9ded5b0a7e7cf38a012d3f57040c88a78742
Simon Delcourt <delcourt.simon@gmail.com>
2019-02-14 16:14:31 +0100

    Use new methods of imio.schedule.

--- src/liege/urban/events/licence_events.py
+++ src/liege/urban/events/licence_events.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from imio.schedule.utils import query_container_tasks
+from imio.schedule.utils import get_container_tasks
 
 from plone import api
 
@@ -9,5 +9,5 @@ def reindex_licence_tasks(licence, event):
     """
     """
     with api.env.adopt_roles(['Manager']):
-        for task in query_container_tasks(licence, the_objects=True):
+        for task in get_container_tasks(licence):
             task.reindexObject(idxs=['shore'])
