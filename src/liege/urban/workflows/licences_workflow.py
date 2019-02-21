564d66b3bd105686c3789b633664dc3bf263b8ca
Simon Delcourt <delcourt.simon@gmail.com>
2019-02-21 10:08:04 +0100

    Add inacceptable state to urban licence workflow.

--- src/liege/urban/workflows/licences_workflow.py
+++ src/liege/urban/workflows/licences_workflow.py
@@ -43,6 +43,15 @@ class DefaultStateRolesMapping(BaseRolesMapping):
             'survey_editors': ('Reader', 'AddressEditor'),
         },
 
+        'inacceptable': {
+            BaseRolesMapping.get_readers: ('Reader',),
+            BaseRolesMapping.get_editors: ('Editor',),
+            'Voirie_editors': ('RoadEditor', 'Reader'),
+            'Voirie_validators': ('RoadEditor', 'Reader'),
+            BaseRolesMapping.get_opinion_editors: ('ExternalReader',),
+            'survey_editors': ('Reader', 'AddressEditor'),
+        },
+
         'retired': {
             BaseRolesMapping.get_readers: ('Reader',),
             BaseRolesMapping.get_editors: ('Editor',),
