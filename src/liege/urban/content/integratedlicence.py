98e367ca9612095ead4bd725a73e4623ffb07276
Simon Delcourt <delcourt.simon@gmail.com>
2019-06-18 13:25:48 +0200

    IntegratedLicence cwatup has no field referenceSPE.

--- src/liege/urban/content/integratedlicence.py
+++ src/liege/urban/content/integratedlicence.py
@@ -4,10 +4,6 @@ from Products.urban.content.licence.IntegratedLicence import IntegratedLicence
 
 # buildlicence and article127 schema should have the same changes
 from liege.urban.content.buildlicence import update_item_schema
-from liege.urban.content.codt_uniquelicence import updateTitle
 
 
 IntegratedLicence.schema = update_item_schema(IntegratedLicence.schema)
-
-
-IntegratedLicence.updateTitle = updateTitle
