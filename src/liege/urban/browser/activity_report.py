ed57da471dda845ab9801b2a69642651ab5f6bd6
Simon Delcourt <delcourt.simon@gmail.com>
2019-08-02 14:17:24 +0200

    Typo.

--- src/liege/urban/browser/activity_report.py
+++ src/liege/urban/browser/activity_report.py
@@ -174,7 +174,7 @@ class LicencesExtractForm(form.Form):
             licence_dict['modification_registry_date'] = licence.getLastModificationRegistry() and str(licence.getLastModificationRegistry().getEventDate()) or ''
             licence_dict['iile_prescription_date'] = licence.getLastIILEPrescription() and str(licence.getLastIILEPrescription().getEventDate()) or ''
             licence_dict['provocation_date'] = licence.getLastProvocation() and str(licence.getLastProvocation().getEventDate()) or ''
-            licence_dict['exploitant_change_date'] = licence.getLastProprietaryChangeEvent()and str(licence.getLastProprietaryChangeEvent.getEventDate()) or ''
+            licence_dict['exploitant_change_date'] = licence.getLastProprietaryChangeEvent() and str(licence.getLastProprietaryChangeEvent().getEventDate()) or ''
             licence_dict['rubrics'] = self.extract_rubrics(licence)
             licence_dict['rubrics_history'] = getattr(licence, 'rubrics_history', [])
 
