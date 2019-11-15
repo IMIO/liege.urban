605858f0161ef05d56d0d5db2779ed766090463e
Simon Delcourt <delcourt.simon@gmail.com>
2019-11-15 15:03:42 +0100

    Find old licences based on creator rather than creation date.

--- src/liege/urban/Extensions/fix_decisions_dates.py
+++ src/liege/urban/Extensions/fix_decisions_dates.py
@@ -11,17 +11,19 @@ def fix_decisions_dates():
     catalog = api.portal.get_tool('portal_catalog')
     licences_brains = catalog(
         object_provides=IGenericLicence.__identifier__,
-        created={
-            'query': [
-                DateTime('18/01/2011'), DateTime('19/03/2017')
-            ],
-            'range': 'minmax'
-        }
+        Creator='admin',
+#        created={
+#            'query': [
+#                DateTime('18/01/2011'), DateTime('19/03/2017')
+#            ],
+#            'range': 'minmax'
+#        }
     )
     for brain in licences_brains:
         licence = brain.getObject()
-        decision_event = licence.getLastTheLicence()
-        if decision_event:
-            if decision_event.getEventDate() and not decision_event.getDecisionDate():
-                decision_event.setDecisionDate(decision_event.getEventDate())
-                print "fixed decision date of licence {}".format(licence.getReference())
+        if hasattr(licence, 'getLastTheLicence'):
+            decision_event = licence.getLastTheLicence()
+            if decision_event:
+                if decision_event.getEventDate() and not decision_event.getDecisionDate():
+                    decision_event.setDecisionDate(decision_event.getEventDate())
+                    print "fixed decision date of licence {}".format(licence.getReference())
