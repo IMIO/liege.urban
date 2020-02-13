5c724f4c3039902f043b6c5dc34d8dbcea2112c2
Simon Delcourt <delcourt.simon@gmail.com>
2020-02-13 14:14:35 +0100

    Fix is_inspector view.

--- src/liege/urban/browser/inspection.py
+++ src/liege/urban/browser/inspection.py
@@ -9,14 +9,14 @@ class IsInspector(object):
     """
 
     def __init__(self, context, request):
-        self.licence = context
+        self.context = context
         self.request = request
 
     def __call__(self):
         """
         """
         current_user = api.user.get_current()
-        is_admin = self.is_admin(current_user)
+        is_admin = api.user.get_permissions(user=current_user, obj=self.context)['Manage portal']
         if is_admin:
             return True
         user_groups = self.get_groups_ids(current_user)
