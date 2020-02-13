239a6c42b81a2b883aa6b4103eb3891f98123de4
Simon Delcourt <delcourt.simon@gmail.com>
2020-02-13 14:03:12 +0100

    URBLIE-366 allow inspectors to refuse their own report submission.

---
+++ src/liege/urban/browser/inspection.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+
+
+class IsInspector(object):
+    """
+    Return is a user is in the inspection groups
+    """
+
+    def __init__(self, context, request):
+        self.licence = context
+        self.request = request
+
+    def __call__(self):
+        """
+        """
+        current_user = api.user.get_current()
+        is_admin = self.is_admin(current_user)
+        if is_admin:
+            return True
+        user_groups = self.get_groups_ids(current_user)
+        if 'inspection_editors' in user_groups or 'inspection_validators' in user_groups:
+            return True
+        return False
