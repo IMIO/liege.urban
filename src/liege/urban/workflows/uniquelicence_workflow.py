b355988b1964861036d00b8471f5904b96bcd3c1
Simon Delcourt <delcourt.simon@gmail.com>
2020-02-25 09:12:30 +0100

    Allow to add contact on final states of UniqueLicence.

---
+++ src/liege/urban/workflows/uniquelicence_workflow.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from liege.urban.wokflows.licences_workflow import DefaultStateRolesMapping
+
+
+class StateRolesMapping(DefaultStateRolesMapping):
+    """
+    Use generic licences rolemapping.
+    The only differences are in the wofklow xml definition.
+    """
