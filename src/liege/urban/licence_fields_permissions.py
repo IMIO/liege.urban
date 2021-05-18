6f8ea9e5365763b19a8eaf3144cda3ae1ecade64
Simon Delcourt <delcourt.simon@gmail.com>
2021-05-18 14:54:48 +0200

    Default read permission is now External reader.

--- src/liege/urban/licence_fields_permissions.py
+++ src/liege/urban/licence_fields_permissions.py
@@ -7,7 +7,7 @@ def set_field_permissions(schema, mapping={}, exceptions=['portal_type', 'id']):
 
     for field in schema.fields():
         if field.getName() not in exceptions:
-            default = ('liege.urban: Internal Reader', 'liege.urban: Internal Editor')
+            default = ('liege.urban: External Reader', 'liege.urban: Internal Editor')
             read_permission, write_permission = mapping.get(field.schemata, default)
             if read_permission:
                 field.read_permission = read_permission
