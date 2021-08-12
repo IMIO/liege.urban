32492972fc24ab06e4b065c585a1ee0528cf2d2c
sdelcourt <simon.delcourt@imio.be>
2021-08-12 15:54:07 +0200

    Typo

--- src/liege/urban/workflows/roaddecree_workflow.py
+++ src/liege/urban/workflows/roaddecree_workflow.py
@@ -13,54 +13,54 @@ class StateRolesMapping(LiegeBase):
         'folder_creation': {
             'administrative_editors': ('Editor', 'AddressEditor'),
             'administrative_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
 
         'public_investigation': {
             'administrative_editors': ('Editor', 'AddressEditor'),
             'administrative_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
 
         'technical_analysis_post_investigation': {
             'fittingout_technicians': ('Editor', ),
             'fittingout_technicians_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
 
         'technical_analysis_validation': {
             'fittingout_technicians': ('Reader',),
             'fittingout_technicians_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
 
         'college_council_passage': {
             'administrative_editors': ('Editor', 'AddressEditor'),
             'administrative_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
 
         'display_in_progress': {
             'administrative_editors': ('Editor', 'AddressEditor'),
             'administrative_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
 
         'authorized': {
             'administrative_editors': ('Editor', 'AddressEditor'),
             'administrative_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
 
         'refused': {
             'administrative_editors': ('Editor', 'AddressEditor'),
             'administrative_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
 
         'abandoned': {
             'administrative_editors': ('Editor', 'AddressEditor'),
             'administrative_validators': ('Contributor',),
-            'urban_readers': ('Reader', 'RoadDecree'),
+            'urban_readers': ('Reader', 'RoadReader'),
         },
     }
