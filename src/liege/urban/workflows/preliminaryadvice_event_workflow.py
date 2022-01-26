c8d1ab11b0a817ea111bc1e504bd8973c3d661b1
sdelcourt <simon.delcourt@imio.be>
2022-01-26 14:42:14 +0100

    SUP-21752. Set correct permissions for environment readers.

--- src/liege/urban/workflows/preliminaryadvice_event_workflow.py
+++ src/liege/urban/workflows/preliminaryadvice_event_workflow.py
@@ -13,28 +13,28 @@ class StateRolesMapping(LocalRoleAdapter):
             'technical_editors': ('Editor',),
             'technical_validators': ('Editor', 'Contributor'),
             'urban_readers': ('Reader', 'RoadReader'),
-            'urban_readers': ('Reader', 'RoadReader'),
+            'environment_readers': ('Reader', 'RoadReader'),
         },
 
         'technical_validation': {
             'administrative_editors': ('Editor',),
             'technical_validators': ('Editor', 'Contributor'),
             'urban_readers': ('Reader', 'RoadReader'),
-            'urban_readers': ('Reader', 'RoadReader'),
+            'environment_readers': ('Reader', 'RoadReader'),
         },
 
         'executive_validation': {
             'administrative_editors': ('Editor',),
             'administrative_validators': ('Editor', 'Contributor'),
             'urban_readers': ('Reader', 'RoadReader'),
-            'urban_readers': ('Reader', 'RoadReader'),
+            'environment_readers': ('Reader', 'RoadReader'),
         },
 
         'preliminary_advice_sent': {
             'administrative_validators': ('Editor', 'Contributor'),
             'technical_validators': ('Editor', 'Contributor'),
             'urban_readers': ('Reader', 'RoadReader'),
-            'urban_readers': ('Reader', 'RoadReader'),
+            'environment_readers': ('Reader', 'RoadReader'),
         },
 
     }
