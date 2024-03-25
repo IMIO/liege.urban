eee6d318119f25de6fff13a1452b7e5083abd46a
Martin Peeters <martin.peeters@affinitic.be>
2024-03-25 16:21:49 +0100

    URB-2956: Fix an issue with zope users on urban homepage

--- src/liege/urban/browser/redirects.py
+++ src/liege/urban/browser/redirects.py
@@ -22,7 +22,10 @@ class UrbanRootRedirects(object):
         if self.user.getId() is None:
             return None
         else:
-            user_groups = api.group.get_groups(user=self.user)
+            try:
+                user_groups = api.group.get_groups(user=self.user)
+            except AttributeError:  # This happen with admin user
+                user_groups = []
             group_ids = [g.id for g in user_groups]
             if 'survey_editors' in group_ids:
                 return 'urban/survey_schedule'
