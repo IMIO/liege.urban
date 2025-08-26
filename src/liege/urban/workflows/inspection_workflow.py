ac1f67cb46926585e88ca035fa82c47898637659
Nicolas Demonté <nicolas@affinitic.be>
2025-08-26 11:42:11 +0200

    Black

--- src/liege/urban/workflows/inspection_workflow.py
+++ src/liege/urban/workflows/inspection_workflow.py
@@ -7,53 +7,47 @@ class StateRolesMapping(LocalRoleAdapter):
     """ """
 
     mapping = {
-        'creation': {
-            'inspection_editors': ('Editor', 'AddressEditor'),
-            'administrative_editors': ('Editor', 'AddressEditor'),
-            'administrative_validators': ('Contributor', 'AddressEditor'),
-            'urban_readers': ('Reader',),
+        "creation": {
+            "inspection_editors": ("Editor", "AddressEditor"),
+            "administrative_editors": ("Editor", "AddressEditor"),
+            "administrative_validators": ("Contributor", "AddressEditor"),
+            "urban_readers": ("Reader",),
         },
-
-        'first_administrative_answer': {
-            'inspection_editors': ('Reader',),
-            'administrative_editors': ('Editor', 'AddressEditor'),
-            'administrative_validators': ('Contributor', 'AddressEditor'),
-            'urban_readers': ('Reader',),
+        "first_administrative_answer": {
+            "inspection_editors": ("Reader",),
+            "administrative_editors": ("Editor", "AddressEditor"),
+            "administrative_validators": ("Contributor", "AddressEditor"),
+            "urban_readers": ("Reader",),
         },
-
-        'analysis': {
-            'inspection_editors': ('Editor', 'AddressEditor'),
-            'inspection_validators': ('Contributor', 'AddressEditor'),
-            'administrative_editors': ('Editor', 'AddressEditor'),
-            'administrative_validators': ('Contributor', 'AddressEditor'),
-            'urban_readers': ('Reader',),
+        "analysis": {
+            "inspection_editors": ("Editor", "AddressEditor"),
+            "inspection_validators": ("Contributor", "AddressEditor"),
+            "administrative_editors": ("Editor", "AddressEditor"),
+            "administrative_validators": ("Contributor", "AddressEditor"),
+            "urban_readers": ("Reader",),
         },
-
-        'administrative_answer': {
-            'inspection_editors': ('Reader',),
-            'administrative_editors': ('Editor', 'AddressEditor'),
-            'administrative_validators': ('Contributor', 'AddressEditor'),
-            'urban_readers': ('Reader',),
+        "administrative_answer": {
+            "inspection_editors": ("Reader",),
+            "administrative_editors": ("Editor", "AddressEditor"),
+            "administrative_validators": ("Contributor", "AddressEditor"),
+            "urban_readers": ("Reader",),
         },
-
-        'inspection_follow_up': {
-            'inspection_editors': ('Editor', 'AddressEditor'),
-            'administrative_editors': ('Editor', 'AddressEditor'),
-            'administrative_validators': ('Contributor', 'AddressEditor'),
-            'urban_readers': ('Reader',),
+        "inspection_follow_up": {
+            "inspection_editors": ("Editor", "AddressEditor"),
+            "administrative_editors": ("Editor", "AddressEditor"),
+            "administrative_validators": ("Contributor", "AddressEditor"),
+            "urban_readers": ("Reader",),
         },
-
-        'ended': {
-            'inspection_editors': ('Editor', 'AddressEditor'),
-            'administrative_editors': ('Editor', 'AddressEditor'),
-            'administrative_validators': ('Contributor', 'AddressEditor'),
-            'urban_readers': ('Reader',),
+        "ended": {
+            "inspection_editors": ("Editor", "AddressEditor"),
+            "administrative_editors": ("Editor", "AddressEditor"),
+            "administrative_validators": ("Contributor", "AddressEditor"),
+            "urban_readers": ("Reader",),
         },
-
-        'frozen_suspension': {
-            'inspection_editors': ('Editor', 'AddressEditor'),
-            'administrative_editors': ('Editor', 'AddressEditor'),
-            'administrative_validators': ('Contributor', 'AddressEditor'),
-            'urban_readers': ('Reader',),
+        "frozen_suspension": {
+            "inspection_editors": ("Editor", "AddressEditor"),
+            "administrative_editors": ("Editor", "AddressEditor"),
+            "administrative_validators": ("Contributor", "AddressEditor"),
+            "urban_readers": ("Reader",),
         },
     }
