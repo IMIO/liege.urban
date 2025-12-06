4d90a72dfed8ec06788d7224450a4da27ba804cf
Martin Peeters <martin.peeters@affinitic.be>
2025-12-07 00:37:00 +0100

    URBBDC-3205: Add `IAddress` behavior for parcels

---
+++ src/liege/urban/behaviors/address.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+from zope import schema
+from liege.urban import UrbanMessage as _
+
+
+class IAddress(Interface):
+
+    street_code = schema.TextLine(
+        title=_(u"Street code"),
+        required=False,
+    )
+
+    street_name = schema.TextLine(
+        title=_(u"Street name"),
+        required=False,
+    )
+
+    street_number = schema.TextLine(
+        title=_(u"Street number"),
+        required=False,
+    )
+
+    zip_code = schema.TextLine(
+        title=_(u"Zip Code"),
+        required=False,
+    )
+
+    address_point = schema.Integer(
+        title=_(u"Address Point"),
+        required=False,
+    )
