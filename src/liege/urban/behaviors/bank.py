cf8126d572ef9f53a806e523e8816211e0c5b562
jimbiscuit <julienchandelle@gmail.com>
2025-12-11 13:13:48 +0100

    URBBDC-3205 Fix behavior not displaying

--- src/liege/urban/behaviors/bank.py
+++ src/liege/urban/behaviors/bank.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from plone.autoform.interfaces import IFormFieldProvider
+from zope.interface import provider
 from zope.interface import Interface
 from zope import schema
 from liege.urban import UrbanMessage as _
+from plone.supermodel import model
 
 
-class IBank(Interface):
+@provider(IFormFieldProvider)
+class IBank(model.Schema):
     shore = schema.Choice(
         title=_(u"Bank"),
         required=False,
