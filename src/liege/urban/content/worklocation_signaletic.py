3a88b1f568618077b3d606ee030a8e4e55615312
Julien Chandelle <julienchandelle@gmail.com>
2025-12-11 11:12:19 +0100

    SUP-49356: Fix string encoding worklocation signaletic (#24)

--- src/liege/urban/content/worklocation_signaletic.py
+++ src/liege/urban/content/worklocation_signaletic.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
+from Products.CMFPlone.utils import safe_unicode
 from Products.urban.interfaces import IWorklocationSignaletic
 
 from zope.i18n import translate
@@ -20,21 +21,21 @@ class LiegeLicenceToWorklocationsSignaletic(object):
         if address_points:
             signaletic = ''
             for address in address_points:
-                zip_code = address.zip_code.encode("utf-8")
+                zip_code = safe_unicode(address.zip_code).encode("utf-8")
                 city = address.getDivisionAlternativeName()
-                city = city and city.split('(')[0].encode('utf-8') or ''
-                street = address.street_name.encode("utf-8")
-                number = address.street_number.encode("utf-8")
-                separator = u"à".encode("utf-8")
+                city = city and safe_unicode(city.split('(')[0]).encode("utf-8") or ''
+                street = safe_unicode(address.street_name).encode("utf-8")
+                number = safe_unicode(address.street_number).encode("utf-8")
+                separator = safe_unicode(u"à").encode("utf-8")
                 if signaletic:
-                    signaletic += ' %s ' % translate('and', 'urban', context=licence.REQUEST).encode('utf8')
+                    signaletic += safe_unicode(' %s ' % translate('and', 'urban', context=licence.REQUEST)).encode("utf-8")
                 if number:
                     signaletic += "%s %s %s %s %s" % (street, number, separator, zip_code, city)
                 else:
                     signaletic += "%s - %s %s" % (street, zip_code, city)
             return signaletic
         else:
-            return licence.getDefaultWorkLocationSignaletic()
+            return safe_unicode(licence.getDefaultWorkLocationSignaletic()).encode("utf-8")
 
     def get_street_and_number(self):
         licence = self.licence
@@ -42,12 +43,12 @@ class LiegeLicenceToWorklocationsSignaletic(object):
         if address_points:
             signaletic = ''
             for address in address_points:
-                street = address.street_name.encode("utf-8")
-                number = address.street_number.encode("utf-8")
+                street = safe_unicode(address.street_name).encode("utf-8")
+                number = safe_unicode(address.street_number).encode("utf-8")
                 if number:
                     signaletic = '{} {} {}'.format(signaletic, street, number)
                 else:
                     signaletic = '{} {}'.format(signaletic, street)
             return signaletic
         else:
-            return licence.getDefaultStreetAndNumber()
+            return safe_unicode(licence.getDefaultStreetAndNumber()).encode("utf-8")
