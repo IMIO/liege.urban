e48b4be108aa52e379463f846f3114ea2a2304ec
Martin Peeters <martin.peeters@affinitic.be>
2026-02-23 20:57:31 +0100

    Back to development: 2.0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='liege.urban',
-    version='2.0.1',
+    version='2.0.2.dev0',
     description="Liège urban profile",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
