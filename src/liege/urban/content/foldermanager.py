4f1fddedad695ac551b1866da1850958a3ca25c7
Martin Peeters <martin.peeters@affinitic.be>
2026-03-02 11:17:21 +0100

    SUP-51197: Fix folder manager title for tests

--- src/liege/urban/content/foldermanager.py
+++ src/liege/urban/content/foldermanager.py
@@ -6,9 +6,12 @@ from Products.urban.FolderManager import FolderManager
 def Title(self):
     """
     """
+    grade = self.getGrade()
+    if not grade or not self.Vocabulary('grade')[0].getValue(grade):
+        return self.id
     title = "{} ({})".format(
         self.getInitials(),
-        self.Vocabulary('grade')[0].getValue(self.getGrade()).encode("utf8"),
+        self.Vocabulary('grade')[0].getValue(grade).encode("utf8"),
     )
     return title
 
