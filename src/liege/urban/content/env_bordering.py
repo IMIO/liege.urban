e78ce8bb38aa460090a5da2d0389b45b6f81d5ad
jimbiscuit <julienchandelle@gmail.com>
2026-03-06 09:36:46 +0100

    SUP-51482 Fix encoding for env bordering title

--- src/liege/urban/content/env_bordering.py
+++ src/liege/urban/content/env_bordering.py
@@ -3,6 +3,7 @@
 from liege.urban.content.genericlicence import updateTitle as base_updateTitle
 
 from Products.urban.content.licence.EnvClassBordering import EnvClassBordering
+from Products.CMFPlone.utils import safe_unicode
 
 
 def updateTitle(self):
@@ -10,10 +11,10 @@ def updateTitle(self):
         Update the title to clearly identify the licence
     """
     base_title = base_updateTitle(self)
-    streets = ', '.join([wl['street'] for wl in self.getWorkLocations()])
-    title = "{} - {}".format(base_title, streets)
+    streets = u", ".join([safe_unicode(wl["street"]) for wl in self.getWorkLocations()])
+    title = u"{} - {}".format(base_title, streets)
     self.setTitle(title)
-    self.reindexObject(idxs=('Title', 'applicantInfosIndex', 'sortable_title', ))
+    self.reindexObject(idxs=("Title", "applicantInfosIndex", "sortable_title", ))
     return title
 
 
