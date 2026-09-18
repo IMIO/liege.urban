71766389d68c30d390d768011bf02530f22253db
Julien Chandelle <julienchandelle@gmail.com>
2026-09-18 09:32:05 +0200

    SUP-49906: Fix street_name unicode in list 220 (#44)

--- src/liege/urban/adapters/urbain220.py
+++ src/liege/urban/adapters/urbain220.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from Products.CMFPlone.utils import safe_unicode
 from Products.urban.interfaces import IToUrbain220Street
-
 from zope.interface import implements
 
 
@@ -18,6 +18,7 @@ class LiegeLicenceToUrbain220Street(object):
             if address.street_name and address.street_code:
                 first_address = address
                 break
-        self.street_name = first_address and first_address.street_name.encode('utf-8')
-        self.street_code = first_address and first_address.street_code.encode('utf-8')
+
+        self.street_name = first_address and safe_unicode(first_address.street_name)
+        self.street_code = first_address and safe_unicode(first_address.street_code)
         self.street_number = first_address and first_address.street_number
